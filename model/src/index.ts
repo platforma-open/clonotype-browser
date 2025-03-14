da6375deff0f455e168cd6074d72a8725b261bf5
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2025-03-14 20:41:05 +0200

    wip

--- model/src/index.ts
+++ model/src/index.ts
@@ -1,23 +1,40 @@
 import type {
-  InferHrefType } from '@platforma-sdk/model';
+  InferHrefType,
+  PlRef } from '@platforma-sdk/model';
 import {
   BlockModel,
+  isPColumnSpec,
   type InferOutputsType,
 } from '@platforma-sdk/model';
 
 type BlockArgs = {
-  input?: string;
+  /** Anchor column from the clonotyping output (must have sampleId and clonotypeKey axes) */
+  inputAnchor?: PlRef;
 };
 
 export const platforma = BlockModel.create('Heavy')
 
   .withArgs<BlockArgs>({})
 
+  .output('inputOptions', (ctx) =>
+    ctx.resultPool.getOptions((spec) => isPColumnSpec(spec)
+      && spec.axesSpec.length === 1
+      && spec.axesSpec[0].name === 'pl7.app/vdj/clonotypeKey'
+      && spec.annotations?.['pl7.app/isAnchor'] === 'true',
+    { includeNativeLabel: true },
+    ),
+    // ctx.resultPool.getSpecs().entries.filter(({ obj: spec }) => isPColumnSpec(spec)
+    //   && spec.axesSpec.length === 1
+    //   && spec.axesSpec[0].name === 'pl7.app/vdj/clonotypeKey'
+    //   && spec.annotations?.['pl7.app/isAnchor'] === 'true',
+    // ),
+  )
+
   .sections((_ctx) => {
     return [{ type: 'link', href: '/', label: 'Main' }];
   })
 
-  .argsValid((ctx) => true)
+  .argsValid((ctx) => false)
 
   .title((ctx) => 'Clonotype Tagger')
 
--- test/src/wf.test.ts
+++ test/src/wf.test.ts
@@ -1,25 +1,30 @@
+import type {
+  BlockArgs,
+  BlockOutputs as MiXCRClonotypingBlockOutputs,
+  platforma as mixcrPlatforma } from '@platforma-open/milaboratories.mixcr-clonotyping-2.model';
 import {
   AlignReport,
   AssembleReport,
-  BlockArgs,
-  BlockOutputs,
-  platforma,
   Qc,
   SupportedPresetList,
-  uniquePlId
-} from '@platforma-open/milaboratories.clonotype-tagger.model';
+  uniquePlId,
+} from '@platforma-open/milaboratories.mixcr-clonotyping-2.model';
 import { awaitStableState, blockTest } from '@platforma-sdk/test';
 import { blockSpec as samplesAndDataBlockSpec } from '@platforma-open/milaboratories.samples-and-data';
-import { BlockArgs as SamplesAndDataBlockArgs } from '@platforma-open/milaboratories.samples-and-data.model';
-import { blockSpec as myBlockSpec } from 'this-block';
-import { InferBlockState, fromPlRef, wrapOutputs } from '@platforma-sdk/model';
+import type { BlockArgs as SamplesAndDataBlockArgs } from '@platforma-open/milaboratories.samples-and-data.model';
+import { blockSpec as clonotypingBlockSpec } from '@platforma-open/milaboratories.mixcr-clonotyping-2';
+import { blockSpec as annotationBlockSpec } from 'this-block';
+import type { platforma } from '@platforma-open/milaboratories.clonotype-tagger.model';
+import type { InferBlockState } from '@platforma-sdk/model';
+import { fromPlRef, wrapOutputs } from '@platforma-sdk/model';
 
 blockTest(
   'simple project',
   { timeout: 55000 },
   async ({ rawPrj: project, ml, helpers, expect }) => {
     const sndBlockId = await project.addBlock('Samples & Data', samplesAndDataBlockSpec);
-    const clonotypingBlockId = await project.addBlock('MiXCR Clonotyping', myBlockSpec);
+    const clonotypingBlockId = await project.addBlock('MiXCR Clonotyping', clonotypingBlockSpec);
+    const annotationBlockId = await project.addBlock('Clonotype Annotation', annotationBlockSpec);
 
     const sample1Id = uniquePlId();
     const metaColumn1Id = uniquePlId();
@@ -28,7 +33,7 @@ blockTest(
     const r1Handle = await helpers.getLocalFileHandle('./assets/small_data_R1.fastq.gz');
     const r2Handle = await helpers.getLocalFileHandle('./assets/small_data_R2.fastq.gz');
 
-    project.setBlockArgs(sndBlockId, {
+    await project.setBlockArgs(sndBlockId, {
       metadata: [
         {
           id: metaColumn1Id,
@@ -36,9 +41,9 @@ blockTest(
           global: false,
           valueType: 'Long',
           data: {
-            [sample1Id]: 2345
-          }
-        }
+            [sample1Id]: 2345,
+          },
+        },
       ],
       sampleIds: [sample1Id],
       sampleLabelColumnLabel: 'Sample Name',
@@ -54,12 +59,12 @@ blockTest(
             data: {
               [sample1Id]: {
                 R1: r1Handle,
-                R2: r2Handle
-              }
-            }
-          }
-        }
-      ]
+                R2: r2Handle,
+              },
+            },
+          },
+        },
+      ],
     } satisfies SamplesAndDataBlockArgs);
     await project.runBlock(sndBlockId);
     await helpers.awaitBlockDone(sndBlockId, 8000);
@@ -68,31 +73,31 @@ blockTest(
 
     const sdnStableState1 = await helpers.awaitBlockDoneAndGetStableBlockState(sndBlockId, 8000);
     expect(sdnStableState1.outputs).toMatchObject({
-      fileImports: { ok: true, value: { [r1Handle]: { done: true }, [r2Handle]: { done: true } } }
+      fileImports: { ok: true, value: { [r1Handle]: { done: true }, [r2Handle]: { done: true } } },
     });
 
     const clonotypingStableState1 = (await awaitStableState(
       clonotypingBlockState,
-      25000
-    )) as InferBlockState<typeof platforma>;
+      25000,
+    )) as InferBlockState<typeof mixcrPlatforma>;
 
     expect(clonotypingStableState1.outputs).toMatchObject({
       inputOptions: {
         ok: true,
         value: [
           {
-            label: 'Dataset 1'
-          }
-        ]
-      }
+            label: 'Dataset 1',
+          },
+        ],
+      },
     });
 
     const presets = SupportedPresetList.parse(
       JSON.parse(
         Buffer.from(
-          await ml.driverKit.blobDriver.getContent(wrapOutputs(clonotypingStableState1.outputs).presets!.handle!)
-        ).toString()
-      )
+          await ml.driverKit.blobDriver.getContent(wrapOutputs(clonotypingStableState1.outputs).presets!.handle),
+        ).toString(),
+      ),
     );
     expect(presets).length.gt(10);
 
@@ -100,78 +105,30 @@ blockTest(
 
     await project.setBlockArgs(clonotypingBlockId, {
       input: clonotypingStableState1Outputs.inputOptions[0].ref,
-      preset: { type: 'name', name: 'milab-human-dna-xcr-7genes-multiplex' }
+      preset: { type: 'name', name: 'milab-human-dna-xcr-7genes-multiplex' },
+      chains: ['IGHeavy', 'TRB'],
     } satisfies BlockArgs);
 
     const clonotypingStableState2 = (await awaitStableState(
       project.getBlockState(clonotypingBlockId),
-      25000
-    )) as InferBlockState<typeof platforma>;
+      25000,
+    )) as InferBlockState<typeof mixcrPlatforma>;
 
-    const outputs2 = wrapOutputs<BlockOutputs>(clonotypingStableState2.outputs);
-    // console.dir(outputs2.sampleLabels, { depth: 5 });
-    // console.log(JSON.stringify([sample1Id]));
+    const outputs2 = wrapOutputs<MiXCRClonotypingBlockOutputs>(clonotypingStableState2.outputs);
     expect(outputs2.sampleLabels![sample1Id]).toBeDefined();
 
     await project.runBlock(clonotypingBlockId);
-    const clonotypingStableState3 = (await helpers.awaitBlockDoneAndGetStableBlockState(
+    const clonotypingStableState3 = (await helpers.awaitBlockDoneAndGetStableBlockState<typeof mixcrPlatforma>(
       clonotypingBlockId,
-      35000
-    )) as InferBlockState<typeof platforma>;
-    const outputs3 = wrapOutputs<BlockOutputs>(clonotypingStableState3.outputs);
-
-    // console.dir(clonotypingStableState3, { depth: 8 });
-
+      35000,
+    ));
+    const outputs3 = wrapOutputs<MiXCRClonotypingBlockOutputs>(clonotypingStableState3.outputs);
     expect(outputs3.reports.isComplete).toEqual(true);
 
-    const qcEntry = outputs3.qc.data[0];
-    expect(qcEntry).toBeDefined();
-
-    const alignJsonReportEntry = outputs3.reports.data.find(
-      (e: any) => e.key[1] === 'align' && e.key[2] === 'json'
-    );
-    const assembleJsonReportEntry = outputs3.reports.data.find(
-      (e: any) => e.key[1] === 'assemble' && e.key[2] === 'json'
-    );
-
-    expect(alignJsonReportEntry).toBeDefined();
-    expect(assembleJsonReportEntry).toBeDefined();
-
-    const alignReport = AlignReport.parse(
-      JSON.parse(
-        Buffer.from(
-          await ml.driverKit.blobDriver.getContent(alignJsonReportEntry!.value!.handle)
-        ).toString('utf8')
-      )
-    );
-    const assembledReport = AssembleReport.parse(
-      JSON.parse(
-        Buffer.from(
-          await ml.driverKit.blobDriver.getContent(assembleJsonReportEntry!.value!.handle)
-        ).toString('utf8')
-      )
-    );
-
-    const qc = Qc.parse(
-      JSON.parse(
-        Buffer.from(await ml.driverKit.blobDriver.getContent(qcEntry!.value!.handle)).toString(
-          'utf8'
-        )
-      )
-    );
-
-    expect(alignReport.aligned).greaterThan(2);
-
-    const clonesPfHandle = wrapOutputs(clonotypingStableState3.outputs).clones!;
-
-    const clonesPfColumnList = await ml.driverKit.pFrameDriver.listColumns(clonesPfHandle);
-
-    // console.dir(clonesPfColumnList[0].spec, { depth: 5 });
-
-    expect(
-      clonesPfColumnList.map(c => c.spec.axesSpec.find((s: any) => s.name === 'pl7.app/vdj/clonotypeKey')).find(Boolean)?.domain
-    ).toHaveProperty("pl7.app/vdj/clonotypeKey/structure");
-
-    expect(clonesPfColumnList).length.to.greaterThanOrEqual(7);
-  }
+    const annotationStableState1 = (await awaitStableState(
+      project.getBlockState(annotationBlockId),
+      25000,
+    )) as InferBlockState<typeof platforma>;
+    console.dir(annotationStableState1, { depth: 8 });
+  },
 );
